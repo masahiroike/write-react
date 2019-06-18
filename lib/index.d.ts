05dcd38030411e4b0b7aed255a7be22810d12f30
masahiro ike <masachako0520@gmail.com>
2019-06-18 15:36:42 +0900

    chore: build環境を整理

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import * as React from "react";
+declare const _default: <P>(component: string | React.FunctionComponent<P> | React.ComponentClass<P, any>, props: P, ...children: React.ReactNode[]) => void;
+export default _default;
